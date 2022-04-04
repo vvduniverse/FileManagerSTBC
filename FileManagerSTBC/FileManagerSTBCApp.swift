50c5d87df27df7f1e86a6715644069b6c5df08cd
Vahtee Boo <vahtee.boo@gmail.com>
2022-04-04 11:38:31 +0300

    Initial Commit

---
+++ FileManagerSTBC/FileManagerSTBCApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileManagerSTBCApp.swift
+//  FileManagerSTBC
+//
+//  Created by Vahtee Boo on 04.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct FileManagerSTBCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
